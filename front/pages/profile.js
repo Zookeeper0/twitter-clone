f63c01bc1441aa5315217c059204413fa20c90c4
eastzoo <dongju010@kakao.com>
2023-02-23 15:48:10 +0900

    ADD : 프로필 페이지

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import AppLayout from '../components/AppLayout'
-import Head from 'next/head';
 
-const profile = () => {
+import React from 'react';
+
+import NicknameEditForm from '../components/NicknameEditForm';
+import AppLayout from '../components/AppLayout';
+import FollowList from '../components/FollowList';
+
+const Profile = () => {
+    const followerList = [{ nickname: 'zookeeper' }, { nickname: '바보' }, { nickname: '노드버드오피셜' }];
+    const followingList = [{ nickname: '제로초' }, { nickname: '바보' }, { nickname: '노드버드오피셜' }];
+
     return (
-        <>
-            <Head>
-                <title>내 프로필 | Twitter</title>
-            </Head>
-            <AppLayout>
-                Hello profile!!
-            </AppLayout>
-        </>
+        <AppLayout>
+            <NicknameEditForm />
+            <FollowList
+                header="팔로잉 목록"
+                data={followingList}
+            />
+            <FollowList
+                header="팔로워 목록"
+                data={followerList}
+            />
+        </AppLayout>
+    );
+};
 
-    )
-}
-
-export default profile
+export default Profile;
